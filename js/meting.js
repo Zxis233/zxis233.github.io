389650e6be6333567423e6dc552cd5ca95ce8636
Esing <esingzhang@stu.xjtu.edu.cn>
2026-04-03 01:19:36 +0800

    Site updated: 2026-04-03 01:19:32

---
+++ js/meting.js
@@ -0,0 +1,88 @@
+"use strict";
+console.log("\n %c MetingJS v0721 By Esing %c\n", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;");
+var aplayers = []
+    , loadMeting = function () {
+        function a(a, t) {
+            var e = {
+                container: a,
+                audio: t,
+                mini: null,
+                fixed: null,
+                autoplay: !1,
+                mutex: !0,
+                lrcType: 3,
+                listFolded: !1,
+                preload: "auto",
+                theme: "#ff80ac",
+                loop: "none",
+                order: "list",
+                volume: 0.3,
+                listMaxHeight: "340px",
+                customAudioType: null,
+                storageName: "metingjs"
+            };
+            if (t.length) {
+                t[0].lrc || (e.lrcType = 0);
+                var r = {};
+                for (var s in e) {
+                    var n = s.toLowerCase();
+                    (a.dataset.hasOwnProperty(n) || a.dataset.hasOwnProperty(s) || null !== e[s]) && (r[s] = a.dataset[n] || a.dataset[s] || e[s],
+                        "true" !== r[s] && "false" !== r[s] || (r[s] = "true" == r[s]))
+                }
+                aplayers.push(new APlayer(r))
+            }
+        }
+        var t = "https://api.baka.plus/meting/?server=:server&type=:type&id=:id&auth=:auth&r=:r";
+        "undefined" != typeof meting_api && (t = meting_api);
+        for (var e = 0; e < aplayers.length; e++)
+            if (!aplayers[e].container.classList.contains("no-destroy"))
+                try {
+                    aplayers[e].destroy()
+                } catch (a) {
+                    console.log(a)
+                }
+        aplayers = [];
+        for (var r = document.querySelectorAll(".aplayer"), s = 0; s < r.length; s++) {
+            (function () {
+                var e = r[s];
+                if (e.classList.contains("no-reload"))
+                    return "continue";
+                if (e.classList.contains("no-destroy") && e.classList.add("no-reload"),
+                    e.dataset.id) {
+                    var n = e.dataset.api || t;
+                    n = n.replace(":server", e.dataset.server),
+                        n = n.replace(":type", e.dataset.type),
+                        n = n.replace(":id", e.dataset.id),
+                        n = n.replace(":auth", e.dataset.auth),
+                        n = n.replace(":r", Math.random());
+
+                    fetch(n)
+                        .then(function (res) {
+                            if (!res.ok) {
+                                throw new Error("Request failed with status " + res.status);
+                            }
+                            return res.json();
+                        })
+                        .then(function (data) {
+                            a(e, data);
+                        })
+                        .catch(function (err) {
+                            console.error("Failed to load Meting data:", err);
+                        });
+
+                } else if (e.dataset.url) {
+                    var l = [{
+                        name: e.dataset.name || e.dataset.title || "Audio name",
+                        artist: e.dataset.artist || e.dataset.author || "Audio artist",
+                        url: e.dataset.url,
+                        cover: e.dataset.cover || e.dataset.pic,
+                        lrc: e.dataset.lrc,
+                        type: e.dataset.type || "auto"
+                    }];
+                    a(e, l)
+                }
+            }
+            )()
+        }
+    };
+document.addEventListener("DOMContentLoaded", loadMeting, !1);
